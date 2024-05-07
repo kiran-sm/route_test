c880e02ae061f74ae9d9532262d38749dec6b37e
Kiran <smkiran.09@gmail.com>
2024-05-07 13:01:22 +0530

    TodoList Complete

--- src/components/todoList/TodoList.jsx
+++ src/components/todoList/TodoList.jsx
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 
 const TodoList = () => {
-  return <div>TodoList</div>;
+  const [userInput, setUserInput] = useState("");
+  const [addList, setAddList] = useState([]);
+  const addListHandler = () => {
+    setAddList([userInput, ...addList]);
+    setUserInput("");
+  };
+  const deleteHandler = (index) => {
+    const updateList = [...addList];
+    updateList.splice(index, 1);
+    setAddList(updateList);
+  };
+  return (
+    <div style={{ margin: "25px 0" }}>
+      <input
+        type="text"
+        style={{ marginRight: "10px" }}
+        onChange={(e) => {
+          setUserInput(e.target.value);
+        }}
+        value={userInput}
+        placeholder="Enter task"
+      />
+
+      <button onClick={addListHandler}>Add List</button>
+      <h3>Added List</h3>
+      <ui>
+        {addList.map((list, index) => (
+          <li key={index} style={{ margin: "25px" }}>
+            {list}{" "}
+            <button onClick={() => deleteHandler(index)}>Delete Task</button>{" "}
+          </li>
+        ))}
+      </ui>
+    </div>
+  );
 };
 
 export default TodoList;
