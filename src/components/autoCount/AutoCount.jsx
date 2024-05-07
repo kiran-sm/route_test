8ffebd47449239490d11f0332a4c4a4e31a71f66
Kiran <smkiran.09@gmail.com>
2024-05-07 15:39:35 +0530

    AutoCount completed

--- src/components/autoCount/AutoCount.jsx
+++ src/components/autoCount/AutoCount.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const AutoCount = () => {
-  return <div>AutoCount</div>;
+  const [count, setCount] = useState();
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCount((i) => (i < 20 ? i + 1 : 1));
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+  return <div>Count: {count}</div>;
 };
 
 export default AutoCount;
--- src/components/dynamicButton/DynamicButton.jsx
+++ src/components/dynamicButton/DynamicButton.jsx
@@ -4,10 +4,7 @@ const DynamicButton = () => {
   const buttons = [];
   for (let i = 0; i <= 20; i++) {
     buttons.push(
-      <button
-        key={i}
-        style={{ background: `#a${i}b`, display: "block", margin: "5px" }}
-      >
+      <button key={i} style={{ background: `#a${i}ab`, margin: "5px" }}>
         Button {i}
       </button>
     );
